248f40cc29df0b99e49398ba118c7f48db78f5cb
sshen9 <sam.shen321@gmail.com>
2020-01-11 13:44:13 -0800

    styled event items form

--- src/app/components/ItemEntry/ItemEntry.jsx
+++ src/app/components/ItemEntry/ItemEntry.jsx
@@ -3,27 +3,40 @@ import "./ItemEntry.css";
 import makeCheckForItem from "../makeCheckForItem/makeCheckForItem";
 
 function ItemEntry(props) {
-	/*should be const?*/ 
-	var {itemName, itemNum, present } = props;
-	//present = 1;
-	let askForData = false;
-	if(itemName == ""){
-		askForData = true;
-	}
-	const askForDataFunc = () => {
-		if(askForData){
+    /*should be const?*/
 
-		}
-	}
-	return (
-		<div>
-			
-			<input type = "checkbox" ></input>
-			<input type="text" name = {itemName} placeholder = {itemName}/>
-			
-			
-		</div>
-	);
+    var { itemName, itemNum, present } = props;
+    //present = 1;
+    let askForData = false;
+    if (itemName == "") {
+        askForData = true;
+    }
+    const askForDataFunc = () => {
+        if (askForData) {
+        }
+    };
+    return (
+        <div>
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <div class="input-group-text">
+                        <input
+                            type="checkbox"
+                            aria-label="Checkbox for following text input"
+                        />
+                    </div>
+                </div>
+                <input
+                    type="text"
+                    class="form-control"
+                    aria-describedby="basic-addon1"
+                />
+                <div className="input-group-append">
+                    <button className="btn btn-primary">x</button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default ItemEntry;
--- src/app/pages/EventItems/EventItems.jsx
+++ src/app/pages/EventItems/EventItems.jsx
@@ -1,56 +1,62 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import ItemEntry from '../../components/ItemEntry/ItemEntry';
-import GetInputForNewItem from '../../components/GetInputForNewItem/GetInputForNewItem'
-
+import ItemEntry from "../../components/ItemEntry/ItemEntry";
+import GetInputForNewItem from "../../components/GetInputForNewItem/GetInputForNewItem";
+import "./EventItems.css";
 
 function EventItems(props) {
-	let m_eventName = "Sbhacks event";
-	let m_date = 12262004;
-	let m_dateString = m_date + " ";
-	m_dateString = m_dateString.slice(0,2) + "/" + m_dateString.slice(2,4) + "/" + m_dateString.slice(4);
-	const [items, setItems] = useState([]);
-	useEffect(() => {
-		setItems([
-			{ itemName: "yerba", itemNum: 1, present: 1 },
-			{ itemName: "mate", itemNum: 2, present: 0},
-		]);
-	}, []);
+    let m_eventName = "Sbhacks event";
+    let m_date = 12262004;
+    let m_dateString = m_date + " ";
+    m_dateString =
+        m_dateString.slice(0, 2) +
+        "/" +
+        m_dateString.slice(2, 4) +
+        "/" +
+        m_dateString.slice(4);
+    const [items, setItems] = useState([]);
+    useEffect(() => {
+        setItems([
+            { itemName: "yerba", itemNum: 1, present: 1 },
+            { itemName: "mate", itemNum: 2, present: 0 }
+        ]);
+    }, []);
 
-	const addItem = () => {
-		setItems((prev) => {
-			return [...prev, {itemName: "", present: false}]
-		})
-	}
-	const i = items.map(
-		(item) => {
-			return <ItemEntry itemName={item.itemName} itemNum={item.itemNum} present = {item.present} />;
-		}
-	);
-	return (
+    const addItem = () => {
+        setItems(prev => {
+            return [...prev, { itemName: "", present: false }];
+        });
+    };
+    const i = items.map(item => {
+        return (
+            <ItemEntry
+                itemName={item.itemName}
+                itemNum={item.itemNum}
+                present={item.present}
+            />
+        );
+    });
+    return (
+        <div className eventNameClass>
+            <div className="card">
+                <h1>Event Items</h1>
+                <h3>Date</h3>
+                <div className="card-body">
+                    {i}
+                    <div>
+                        <a
+                            className="btn btn-large btn-success"
+                            onClick={addItem}
+                        >
+                            Add Item
+                        </a>
+                    </div>
+                </div>
+            </div>
 
-
-		<div className eventNameClass>
-			<h1>{m_eventName}</h1>
-			<header className dateStringClass>
-				<h1>{m_dateString}</h1>
-				<ul>
-					<li>
-						{i}
-					</li>
-				</ul>
-				<div>
-					<a className="btn btn-large btn-success" onClick={addItem} >Add Item</a>
-				</div>
-				
-				<Link to="/myevents">Back</Link>
-
-			</header>
-			
-		</div>
-		
-			
-	);
+            <Link to="/myevents">Back</Link>
+        </div>
+    );
 }
 
 export default EventItems;

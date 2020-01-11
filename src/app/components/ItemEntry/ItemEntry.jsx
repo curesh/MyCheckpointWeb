fbfb4db48eab8cb5fcb7d0d8227bb9ca5b00e0c8
Chandra Suresh <chandra.b.suresh@gmail.com>
2020-01-11 00:55:42 -0800

    Prototyping EntryItems page

--- src/app/components/ItemEntry/ItemEntry.jsx
+++ src/app/components/ItemEntry/ItemEntry.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import "./ItemEntry.css";
 
-function ItemEntry() {
-	return <div></div>;
+function ItemEntry(props) {
+	const {itemName, itemNum, present } = props;
+
+	return (
+		<div>
+			<ul>
+				<li>
+					{itemName}
+				</li>
+				<li>
+					{itemNum}
+				</li>
+				<li>
+					{present}
+				</li>
+			</ul>
+		</div>
+	);
 }
 
 export default ItemEntry;
